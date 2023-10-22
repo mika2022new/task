9ffef93d891413025ab2e30ddedaaf05674d1435
Trubnikov Sergey <trubnikovsergey1@gmail.com>
2023-10-22 19:59:14 +0300

    feat: edit layout

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -10,20 +10,23 @@ const Layout = () => {
         </div>
         <div className="body">
           <ul className="menu">
-            <li className="item">
-              <Link to="/arrival">Приход</Link>
+            <li className="menu__avatar">
+              <img src="image/avatar.png" alt="avatar" />
             </li>
-            <li className="item">
-              <Link to="/groups">Группы</Link>
+            <li className="menu__item">
+              <Link to="/arrival">ПРИХОД</Link>
             </li>
-            <li className="item">
-              <Link to="/goods">Продукты</Link>
+            <li className="menu__item">
+              <Link to="/groups">ГРУППЫ</Link>
             </li>
-            <li className="item">
-              <Link to="/users">Пользователи</Link>
+            <li className="menu__item">
+              <Link to="/goods">ПРОДУКТЫ</Link>
             </li>
-            <li className="item">
-              <Link to="/settings">Настройки</Link>
+            <li className="menu__item">
+              <Link to="/users">ПОЛЬЗОВАТЕЛИ</Link>
+            </li>
+            <li className="menu__item">
+              <Link to="/settings">НАСТРОЙКИ</Link>
             </li>
           </ul>
           <div className="content">
